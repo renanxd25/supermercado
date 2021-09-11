42f6203090515a0c3622a36a493a67cd888a3669
renanxd25 <renanxd7x@hotmail.com>
2021-09-10 21:56:03 -0300

    primeiro commit

---
+++ produtos/produto.js
@@ -0,0 +1,32 @@
+export default class Produto{
+    #nome;
+    #valor;
+    #codigo;
+    #tipo;
+
+    constructor(nome, valor, codigo, tipo){
+        this.#nome = nome;
+        this.#valor = valor;
+        this.#codigo = codigo;
+        this.#tipo = tipo;
+    }
+
+    aplicarDesconto(){
+        return this.#valor - this.#valor * 0.1;
+    }
+
+    get nome(){
+        return this.#nome;
+    }
+
+    get valor(){
+        return this.#valor
+    }
+    get codigo(){
+        this.#codigo;
+    }
+    
+    get tipo(){
+        this.#tipo;
+    }
+}
